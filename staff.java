5718e2f935693585df994093cd1bcffd8567ef09
NGUYỄN QUANG HUY <107240800+iLoveQueChi@users.noreply.github.com>
2024-05-06 15:43:40 +0700

    Update staff.java

--- staff.java
+++ staff.java
@@ -5,10 +5,9 @@ public class staff extends employee
 	private String duty;
 
 	// Constructor
-	public staff( String employeeID, String fullName, double allowance, double salaryCoefficient, String department, String duty, int workingDay)
+	public staff(String employeeID, String fullName, double salaryCoefficient, String department, String duty, int workingDay)
 	{
-		super(fullName, employeeID, allowance, salaryCoefficient, workingDay);
-
+		super(employeeID, fullName, salaryCoefficient, workingDay);
 		this.department = department;
 		this.duty = duty;
 	}
@@ -24,17 +23,6 @@ public class staff extends employee
 		return this.duty;
 	}
 
-	public double getAllowance()
-	{
-		if(this.duty.equals("Excutive Staff"))
-			super.allowance = 500;
-		else if(this.duty.equals("Deputy head of department"))
-			super.allowance = 100;
-		else if(this.duty.equals("Head of department"))
-			super.allowance = 1300;
-
-		return super.allowance;
-	}
 
 	// Setter
 	public void setDepartment(String newDepartment)
@@ -51,12 +39,27 @@ public class staff extends employee
 	@Override
 	public double getSalary()
 	{
-		return super.salaryCoefficient*830 + super.allowance + workingDay*30;
+		return super.salaryCoefficient*830 + getAllowance() + workingDay*30;
 	}
 
 	@Override
 	public String toString()
 	{
-		return super.toString() + "_" + this.department + "_" + this.duty;
+		return super.toString() + "_" + getAllowance() + "_" + this.department + "_" + this.duty;
 	}
+
+	@Override
+	public double getAllowance()
+	{
+		int allowance = 0;
+		if(this.duty.equals("Excutive Staff"))
+			allowance = 500;
+		else if(this.duty.equals("Deputy head of department"))
+			allowance = 100;
+		else if(this.duty.equals("Head of department"))
+			allowance = 1300;
+
+		return allowance;
+	}
+	//
 }
