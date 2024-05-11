addeae525f4fe71b56f4de74d64a3a52fd96e70d
NGUYỄN QUANG HUY <107240800+loveCiForever@users.noreply.github.com>
2024-05-11 21:35:52 +0700

    Update employee.java

--- employee.java
+++ employee.java
@@ -1,6 +1,4 @@
-
-public abstract class employee
-{
+public abstract class employee {
 	// Attributes
 	protected String employeeID;
 	protected String fullName;
@@ -10,8 +8,7 @@ public abstract class employee
 	// So we dont put it the constructor and initialize it as a attributes
 
 	// Contrustor
-	public employee(String employeeID, String fullName, double salaryCoefficient, int workingDay)
-	{
+	public employee(String employeeID, String fullName, double salaryCoefficient, int workingDay) {
 		this.employeeID = employeeID;
 		this.fullName = fullName;
 		this.salaryCoefficient = salaryCoefficient;
@@ -19,54 +16,20 @@ public abstract class employee
 	}
 
 	// Getter
-	public String getEmployeeID()
-	{
-		return this.employeeID;
-	}
-
-	public String getFullName()
-	{
-		return this.fullName;
-	}
-
-	public double getSalaryCoefficient()
-	{
-		return this.salaryCoefficient;
-	}
-
-	public int getWorkingDay()
-	{
-		return this.workingDay;
-	}
-
+	public String getEmployeeID() {return this.employeeID;}
+	public String getFullName() {return this.fullName;}
+	public double getSalaryCoefficient() {return this.salaryCoefficient;}
+	public int getWorkingDay() {return this.workingDay;}
 
 	// Setter
-	public void setFullName(String newName)
-	{
-		this.fullName = newName;
-	}
-
-	public void setEmployeeID(String newID)
-	{
-		this.employeeID = newID;
-	}
-
-	public void setSalaryCoefficient(double newSalaryCoefficient)
-	{
-		this.salaryCoefficient = newSalaryCoefficient;
-	}
-
-	public void setWorkingDay(int newWorkingDay)
-	{
-		this.workingDay = newWorkingDay;
-	}
+	public void setFullName(String newName) {this.fullName = newName;}
+	public void setEmployeeID(String newID) {this.employeeID = newID;}
+	public void setSalaryCoefficient(double newSalaryCoefficient) {this.salaryCoefficient = newSalaryCoefficient;}
+	public void setWorkingDay(int newWorkingDay) {this.workingDay = newWorkingDay;}
 
 	//
 	@Override
-	public String toString() 
-	{
-		return this.employeeID + "_" + this.fullName + "_" + this.salaryCoefficient + "_" + this.workingDay;
-	}
+	public String toString() {return this.employeeID + "_" + this.fullName + "_" + this.salaryCoefficient + "_" + this.workingDay;}
 
 	public abstract double getSalary();
 	public abstract double getAllowance();
