570f8bbec4961a42c349b8db02091c8011290cd9
NGUYỄN QUANG HUY <107240800+loveCiForever@users.noreply.github.com>
2024-05-11 21:36:20 +0700

    Update staff.java

--- staff.java
+++ staff.java
@@ -1,56 +1,31 @@
-public class staff extends employee
-{
+public class staff extends employee {
 	// Attributes
 	private String department;
 	private String duty;
 
 	// Constructor
-
-	public staff(String employeeID, String fullName, double salaryCoefficient, int workingDay, String department, String duty)
-	{
+	public staff(String employeeID, String fullName, double salaryCoefficient, int workingDay, String department, String duty) {
 		super(employeeID, fullName, salaryCoefficient, workingDay);
 		this.department = department;
 		this.duty = duty;
 	}
 
 	// Getter
-	public String getDepartment()
-	{
-		return this.department;
-	}
-
-	public String getDuty()
-	{
-		return this.duty;
-	}
+	public String getDepartment() {return this.department;}
+	public String getDuty() {return this.duty;}
 
 	// Setter
-	public void setDepartment(String newDepartment)
-	{
-		this.department = newDepartment;
-	}
-
-	public void setDuty(String newDuty)
-	{
-		this.duty = newDuty;
-	}
-
+	public void setDepartment(String newDepartment) {this.department = newDepartment;}
+	public void setDuty(String newDuty) {this.duty = newDuty;}
 
 	@Override
-	public double getSalary()
-	{
-		return super.salaryCoefficient*830000 + getAllowance() + workingDay*50;
-	}
+	public double getSalary() {return super.salaryCoefficient*830000 + getAllowance() + workingDay*50;}
 
 	@Override
-	public String toString() // this.employeeID + "_" + this.fullName + "_" + this.salaryCoefficient + "_" + this.workingDay + "_" + this.department + "_" + this.duty;
-	{
-		return super.toString() + "_" + this.department + "_" + this.duty + "_" + getAllowance();
-	}
+	public String toString() {return super.toString() + "_" + this.department + "_" + this.duty + "_" + getAllowance();}
 
 	@Override
-	public double getAllowance()
-	{
+	public double getAllowance() {
 		int allowance = 0;
 		if(this.duty.equals("Excutive Staff"))
 			allowance = 500000;
